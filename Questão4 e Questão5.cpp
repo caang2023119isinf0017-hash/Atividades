cc01a5c88b8db21babe839047c024bd376da5ac0
caang2023119isinf0017-hash <caang.2023119isinf0017@aluno.ifpi.edu.br>
2025-12-03 19:59:45 -0300

    Update Questão4 e Questão5.cpp

--- Questão4 e Questão5.cpp
+++ Questão4 e Questão5.cpp
@@ -2,20 +2,23 @@
 	Possua um vetor de 5 números inteiros
 	Peça ao usuário para digitar os valores
 	Exiba todos os valores digitados*/
-int num[5];
-std::cout<<"Digite 5 números inteiros:"<<std::endl;
-for(int r=0; r<5; r++){
-	std::cout<<r+1<<"º número:";
-	std::cin>>num[r];}
-std::cout<<"Números digitados: "<<std::endl;
-for(int e=0; e<5; e++){
-	std::cout<<num[e]<<std::endl;}
-	
+#include <iostream>
+int main{
+	int num[5];
+	std::cout<<"Digite 5 números inteiros:"<<std::endl;
+	for(int r=0; r<5; r++){
+		std::cout<<r+1<<"º número:";
+		std::cin>>num[r];}
+	std::cout<<"Números digitados: "<<std::endl;
+	for(int e=0; e<5; e++){
+		std::cout<<num[e]<<std::endl;}
+		
 /*5-Usando o vetor do exercício anterior, calcule e exiba:
 A soma total dos números
 A média aritmética dos valores*/
-int soma = 0;
-for(int m=0; m<5; m++){
-	soma = soma + num[m];}
-std::cout<<"Soma dos números: "<<soma<<std::endl;
-std::cout<<"Média dos números: "<<soma/5<<std::endl;
+	int soma = 0;
+	for(int m=0; m<5; m++){
+		soma = soma + num[m];}
+	std::cout<<"Soma dos números: "<<soma<<std::endl;
+	std::cout<<"Média dos números: "<<soma/5<<std::endl;
+}
