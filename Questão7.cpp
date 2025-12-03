287e2845c78e5c516efb568761321b0663980fa1
caang2023119isinf0017-hash <caang.2023119isinf0017@aluno.ifpi.edu.br>
2025-12-03 20:03:01 -0300

    Update Questão7.cpp

--- Questão7.cpp
+++ Questão7.cpp
@@ -2,23 +2,24 @@
 	Se x está no vetor
 	Em qual posição ele aparece pela primeira vez.*/
 	
-
-int num[10];
-for(int r=0; r<10; r++){
-	std::cout<<r+1<<"º número:";
-	std::cin>>num[r];}
-int busca = 0;
-std::cout<<"PESQUISA\nDigite um número para verificar se ele está no vetor: ";
-std::cin>>busca;
-int para =1;
-int indice = 0;
-while(para < 11){
-	if(busca == num[indice]){
-		std::cout<<"O número está na posição índice "<<indice<<" do vetor.";
-		para = 11;}
-	else if(busca != num[indice]){
-		std::cout<<"O número não está no índice "<<indice<<std::endl;
-		indice++;
-		para++;
-		}}
+#include <iostream>
+int main{
+	int num[10];
+	for(int r=0; r<10; r++){
+		std::cout<<r+1<<"º número:";
+		std::cin>>num[r];}
+	int busca = 0;
+	std::cout<<"PESQUISA\nDigite um número para verificar se ele está no vetor: ";
+	std::cin>>busca;
+	int para = 1;
+	int indice = 0;
+	while(para < 11){
+		if(busca == num[indice]){
+			std::cout<<"O número apareceu pela primeira vez no índice "<<indice<<" do vetor.";
+			para = 11;}
+		else if(busca != num[indice]){
+			std::cout<<"O número não está no índice "<<indice<<std::endl;
+			indice++;
+			para++;
+			}}
 }
