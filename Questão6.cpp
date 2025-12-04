79a0041063d94c42ae08b859a5dad5a35e32668e
caang2023119isinf0017-hash <caang.2023119isinf0017@aluno.ifpi.edu.br>
2025-12-03 21:12:00 -0300

    Update Questão6.cpp

--- Questão6.cpp
+++ Questão6.cpp
@@ -1,3 +1,35 @@
 /*6-Leia 8 números inteiros para dentro de um vetor e determine:
 	O maior valor e em qual posição ele está
 	O menor valor e em qual posição ele está*/
+
+#include <iostream>
+
+int main() {
+    int numeros[8];
+
+    std::cout << "Digite 8 números inteiros:"<<std::endl;
+    for (int i = 0; i < 8; i++) {
+        std::cout << "Número " << i + 1 << ": ";
+        std::cin >> numeros[i];
+    }
+
+    int maior = numeros[0];
+    int posicao_maior = 0;
+
+    int menor = numeros[0];
+    int posicao_menor = 0;
+
+    for (int i = 1; i < 8; i++) {
+        if (numeros[i] > maior) {
+            maior = numeros[i];
+            posicao_maior = i;
+        }
+        if (numeros[i] < menor) {
+            menor = numeros[i];
+            posicao_menor = i;
+        }
+    }
+
+    std::cout << "Maior valor: " << maior << ". Posição: " << posicao_maior << ".";
+    std::cout << "Menor valor: " << menor << ". Posição: " << posicao_menor << ".";
+}
